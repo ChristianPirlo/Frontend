d914e252163351bfb37842c8f5e9ca49b9f3838c
Christian Pirlo <andrea.christian@si.ukdw.ac.id>
2023-11-22 17:26:41 +0700

    Razor Class Lobary

---
+++ Models/Sampah.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Models
+{
+    public class Sampah
+    {
+        public int Id { get; set; }
+         public string Nama { get; set; }
+         public string Jenis { get; set; }
+         public double Berat { get; set; }
+    }
+}
